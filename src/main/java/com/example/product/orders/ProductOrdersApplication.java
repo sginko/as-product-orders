2868a7666d1899e4763673e3e7b4e5353354df01
sginko <s.ginkota@gmail.com>
2024-05-06 17:46:17 +0200

    Initial commit

---
+++ src/main/java/com/example/product/orders/ProductOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.example.product.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrdersApplication.class, args);
+	}
+
+}
